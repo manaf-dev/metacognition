255662d12db2b422ac4727f464992de68b44dbbf
Manaf <manaf18.m@gmail.com>
2024-09-26 11:06:07 +0000

    Editted signup view.

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             print("Account created successfully")
             return redirect("login")
         else:
--- metacognition/settings.py
+++ metacognition/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_tq56tnv#f+@#$ff+4lyg-kxkve8a&6l10mf1&=^8)sxf8@8dl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".pythonanywhere.com", "127.0.0.1:8000"]
+ALLOWED_HOSTS = [".pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
