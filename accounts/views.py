5522865609fd51a4b6a161719b0ed78e9c6cf5f7
Manaf <manaf18.m@gmail.com>
2024-09-26 11:19:31 +0000

    Added signup  form to signup view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
             print("Account created successfully")
